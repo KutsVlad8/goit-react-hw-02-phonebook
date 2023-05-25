626d78aa04bfac46b3426d0e3abf9ee8a074e8c2
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-05-26 00:18:14 +0300

    update
    add form for saved contacts

---
+++ src/components/App/App.jsx
@@ -0,0 +1,97 @@
+import { Component } from 'react';
+import {
+  Container,
+  Title,
+  FormContainer,
+  Form,
+  Label,
+  Input,
+  Button,
+} from './App.styled';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  nameInputNameId = nanoid(2);
+  nameInputNumberId = nanoid(2);
+
+  handleChange = event => {
+    this.setState({
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+    this.setState({
+      [event.currentTarget.number]: event.currentTarget.value,
+    });
+  };
+
+  handleSubmit = event => {
+    const contact = {
+      id: nanoid(2),
+      name: this.state.name,
+      number: this.state.number,
+    };
+
+    event.preventDefault();
+    this.state.contacts.push(contact);
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Title>PhoneBook</Title>
+
+        <FormContainer>
+          <Form onSubmit={this.handleSubmit}>
+            <Label htmlFor={this.nameInputNameId}>Name</Label>
+            <Input
+              id={this.nameInputNameId}
+              onChange={this.handleChange}
+              type="text"
+              name="name"
+              value={this.state.name}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+
+            <Label htmlFor={this.nameInputNumberId}>Number</Label>
+            <Input
+              id={this.nameInputNumberId}
+              onChange={this.handleChange}
+              type="text"
+              name="number"
+              value={this.state.number}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+
+            <Button type="submit">add contact</Button>
+          </Form>
+        </FormContainer>
+
+        <Title>Contacts</Title>
+
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      </Container>
+    );
+  }
+}
+
+export default App;
